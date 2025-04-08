3761b0602f949dc43130bce1a402191d6d338d67
Farhat <f190886@mail.ru>
2025-04-08 12:15:54 +0400

    delete comment

--- conftest.py
+++ conftest.py
@@ -6,27 +6,27 @@ from generators.entity import EntityGenerator
 
 @pytest.fixture()
 def create_entity():
-    """Фикстура для создания сущности (предусловие)."""
+
     create_endpoint = CreateObject()
     payload = EntityGenerator.random()
     create_endpoint.create_entity(payload)
     yield create_endpoint.response_txt, payload
-    # Постусловие: удаление сущности
+
     delete_endpoint = DeleteObject()
     delete_endpoint.delete_entity_by_id(create_endpoint.response_txt)
 
 
 @pytest.fixture()
 def create_multiple_entities():
-    """Фикстура для создания нескольких сущностей"""
+
     create_endpoint = CreateObject()
     entities = []
-    for _ in range(3):  # Создаём 3 сущности
+    for _ in range(3):
         payload = EntityGenerator.random()
         create_endpoint.create_entity(payload)
         entities.append((create_endpoint.response_txt, payload))
-    yield entities  # Возвращаем список (ID, payload)
-    # Постусловие: удаление всех созданных сущностей
+    yield entities
+
     delete_endpoint = DeleteObject()
     for entity_id, _ in entities:
         delete_endpoint.delete_entity_by_id(entity_id)
